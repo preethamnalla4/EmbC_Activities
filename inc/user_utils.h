9530c1d2eb9687bbeb9607dbc60d8d22d7ef13c4
Preetham Nalla <62927793+preethamnalla4@users.noreply.github.com>
2021-04-30 02:57:56 +0530

    Add files via upload

---
+++ inc/user_utils.h
@@ -0,0 +1,24 @@
+#ifndef __USER_UTILS_H__
+#define __USER_UTILS_H__
+/**
+ * @file user_utils.h
+ * @author Preethamnalla ()
+ * @brief User defined utilities used across the project
+ * @version 0.1
+ * @date 2021-04-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <project_config.h>
+
+
+/**
+ * @brief Function to generate delay in micro seconds
+ * 
+ * 
+ * 
+ */
+void delay_ms(uint32_t delay_time);
+
+#endif /* __USER_UTILS_H__ */
